dd9198ed01bb3b45a68e7fde79c2df659a0f7ec2
Casper Zmuda <zmudakacper03@gmail.com>
2022-05-03 20:01:26 +0200

    created baseplate

---
+++ src/js/script.js
@@ -0,0 +1,22 @@
+'use strict'
+
+console.log('JS connected...')
+
+// let variable1, variable2, variable3
+
+// const main = () => {
+//   prepareDOMElements()
+//   prepareDOMEvents()
+// }
+
+// const prepareDOMElements = () => {
+//   variable1 = document.querySelector('.class')
+//   variable2 = document.querySelector('.class')
+//   variable3 = document.querySelector('.class')
+// }
+
+// const prepareDOMEvents = () => {
+
+// }
+
+// document.addEventListener('DOMContentLoaded', main)
